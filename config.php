b5dee93ea1c3ca8a5660832d8d75d1465a6bcc62
EmirCtis0 <emirinaner@gmail.com>
2024-05-24 22:24:55 +0300

    Ctis-256 team project

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// example : ahmet.yilmaz7654@ug.bilkent.edu.tr
+const EMAIL = 'ali.eray@ug.bilkent.edu.tr' ;
+
+// your password
+const EPASSWORD = 'b9u18QcT' ;
+
+// your fullname
+// example : Ahmet Yılmaz
+const FULLNAME = "Ali Eray" ;
+
+// About email : Recepient email address and Subject field
+const TO = "erayali2003@gmail.com" ;   // ahmet.yilmaz7654@ug.bilkent.edu.tr
+const SUBJECT = "CTIS 256 Test" ;
